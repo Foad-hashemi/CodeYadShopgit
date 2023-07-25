58bb4759e4c6e3189566508a0d310c0c68b03852
foadhashemi <foadhashemi.channel@gmail.com>
2023-07-25 10:52:51 +0330

    add register system

---
+++ Accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name = "Accounts"
+urlpatterns = [
+    path('register', views.Register.as_view(), name="register")
+]
--- Accounts/views.py
+++ Accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import View, FormView
+from django import forms
+from django.forms.utils import ValidationError
+from .forms import RegisterForm
 
-# Create your views here.
+
+class Register(FormView):
+    template_name = 'Accounts/register.html'
+    form_class = RegisterForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
